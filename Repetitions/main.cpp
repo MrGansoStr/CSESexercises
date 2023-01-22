30e463558d23839b54602b177ef2d08e55018b76
MrGansoStr <ccounttnro1@gmail.com>
2023-01-22 12:25:34 -0500

    problems

---
+++ Repetitions/main.cpp
@@ -0,0 +1,27 @@
+//https://cses.fi/problemset/task/1069
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  string DNA;
+  getline(cin, DNA);
+  vector <int> arr;
+  int count = 1;
+  for(int i = 0; i < DNA.length(); i++){
+    if(DNA[i] == DNA[i + 1]){
+      count++;
+    }
+    else{
+      arr.push_back(count);
+      count = 1;
+    }
+  }
+  sort(arr.begin(), arr.end());
+  cout<< arr[arr.size()- 1];
+
+  return 0;
+}
